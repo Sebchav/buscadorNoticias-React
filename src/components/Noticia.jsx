f2ccc5a9583690f94102747af053c14670effb48
Chava <schavarria13@alumnos.uaq.mx>
2023-06-06 16:08:55 -0600

    Card

--- src/components/Noticia.jsx
+++ src/components/Noticia.jsx
@@ -1,7 +1,19 @@
+import 
+{ Card, CardActions, CardContent,
+ CardMedia, Link, Typography, Grid } from "@mui/material"
 
 const Noticia = ({noticia}) => {
+
+  const { urlToImage, url, title, description, source } = noticia;
+
   return (
-    <div>Noticia</div>
+    <Card>
+      <CardMedia 
+        component="img"
+        alt={`Imagen de la noticia ${title}`}
+        image={urlToImage}
+      />
+    </Card>
   )
 }
 
