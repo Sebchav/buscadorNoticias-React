216467fbced0453418d45044043da896a25c6db6
Sebastián Chavarría <schavarria13@alumnos.uaq.mx>
2023-06-10 11:54:57 -0600

    Project 23 finished

--- src/components/ListadoNoticias.jsx
+++ src/components/ListadoNoticias.jsx
@@ -1,11 +1,16 @@
 import Grid from "@mui/material/Grid"
 import Typography from "@mui/material/Typography"
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 import useNoticias from "../hooks/useNoticias"
 import Noticia from "./Noticia"
 
 const ListadoNoticias = () => {
 
-    const {noticias} = useNoticias();
+    const {noticias, totalNoticias, handleChangePagina, pagina} = useNoticias();
+    
+    const totalPaginas = Math.ceil(totalNoticias / 20)
+
 
   return (
     <>
@@ -18,7 +23,10 @@ const ListadoNoticias = () => {
             Últimas Noticias
         </Typography>
 
-        <Grid>
+        <Grid
+            container
+            spacing={2}
+        >
             {noticias.map(noticia => (
                 <Noticia 
                     key={noticia.url}
@@ -26,6 +34,23 @@ const ListadoNoticias = () => {
                 />
             ))}
         </Grid>
+
+        <Stack
+            sx={{
+                marginY:5 
+            }}
+            spacing={2}
+            direction={"row"}
+            justifyContent={"center"}
+            alignItems={"center"}
+        >
+            <Pagination
+                count={totalPaginas}
+                color="primary" 
+                onChange={handleChangePagina}
+                page={pagina}
+            />
+        </Stack>
     </>
   )
 }
--- src/components/Noticia.jsx
+++ src/components/Noticia.jsx
@@ -1,19 +1,53 @@
+/* eslint-disable react/prop-types */
 import 
 { Card, CardActions, CardContent,
  CardMedia, Link, Typography, Grid } from "@mui/material"
 
+// eslint-disable-next-line react/prop-types
 const Noticia = ({noticia}) => {
 
+  // eslint-disable-next-line react/prop-types
   const { urlToImage, url, title, description, source } = noticia;
-
+  
   return (
-    <Card>
-      <CardMedia 
-        component="img"
-        alt={`Imagen de la noticia ${title}`}
-        image={urlToImage}
-      />
-    </Card>
+    <Grid item md={6} lg={4}>
+        <Card >
+            {urlToImage && (
+              <CardMedia 
+              component="img"
+              alt={`Imagen de la noticia ${title}`}
+              image={urlToImage}
+              height={"250"}
+              />
+            )}
+
+            <CardContent>
+                <Typography variant="body1" color="error">
+                  {source.name}
+                </Typography>
+                <Typography variant="h5" component="div">
+                  {title}
+                </Typography>
+                <Typography variant="body2" >
+                  {description}
+                </Typography>
+            </CardContent>
+
+            <CardActions>
+              <Link
+                href={url}
+                target="_blank"
+                variant="button"
+                width={"100%"}
+                textAlign={"center"}
+                sx={{
+                  textDecoration: "none"
+                }}
+              >Leer Noticia</Link>
+            </CardActions>
+        </Card>
+    </Grid>
+    
   )
 }
 
